3d83b335b7a35c265a25a9b38e8c39c5d39b8bca
Shoumik3011 <shoumik.bhattacharya30@gmail.com>
2020-04-26 13:45:53 +0530

    First Commit

---
+++ Flair Detector/__init__.py
@@ -0,0 +1,7 @@
+from .scraper import parse_submission
+from .scraper import yield_submission_from
+from .utils import get_soup
+from .utils import normalize_text
+from .utils import now
+from .utils import unixtime_to_datetime
+from .utils import strf_to_datetime
---
+++ app.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, url_for, flash, redirect ,request
+from forms import FlairForm
+from model import get_flair
+from flask import Response
+from flask_json import FlaskJSON, JsonError, json_response, as_json
+import pickle
+import os
+import json
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='351d8ebafda1892160d5231ce2d11ab1'
+
+posts = []
+
+
+@app.route("/",methods=['GET', 'POST'])
+@app.route("/home", methods=['GET', 'POST'])
+def home():
+    global posts
+    if len(posts)==10:
+        posts=[]
+    form = FlairForm()
+    flag=False
+    if form.validate_on_submit():
+        d={}
+        d['flair']=get_flair(form.URL.data)
+        d['URL']=form.URL.data
+        print("Type : ",type(form.URL.data))
+        print("Data : ",(form.URL.data))
+
+        
+
+        posts=[d]+posts
+        return redirect(url_for('Flairs_Detected'))
+    return render_template('Flair.html', form=form)
+
+
+@app.route("/Flairs_Detected")
+def Flairs_Detected():
+    return render_template('Flair_detected.html',posts=posts)
+
+@app.route("/data_ana1")
+def data_ana1():
+    return render_template('data_analysis1.html')
+
+@app.route("/data_ana2")
+def data_ana2():
+    return render_template('data_analysis2.html')
+
+
+
+@app.route("/getfile", methods=['GET', 'POST'])
+def getfile():
+
+    if request.method == 'POST':
+
+            tf = request.files["myfile"]
+            files=tf.read().decode("utf-8")
+            
+            lines=files.split("\n")
+            jsonfile={}
+            
+            for i in range(len(lines)):
+                 prediction=get_flair(lines[i])
+                 jsonfile[lines[i]]=prediction       
+                    
+            
+            file = open('automation_json.pkl', 'wb')
+            jsonfile = json.dumps(jsonfile)
+
+            # dump information to that file
+            pickle.dump(jsonfile, file)
+            # close the file
+            file.close()
+            return Response(jsonfile,
+                       mimetype="text/json",
+                       headers={"Content-Disposition":
+                                    "attachment;filename=automation_result.json"})
+        
+
+            
+    return render_template("automation.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
